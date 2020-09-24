7a170e0bed28660017515d884f6cc2defd9c5727
Diego <ldrc161997@gmail.com>
2020-09-23 23:02:20 -0600

    ejercicio de sumatoria cubica resuelto y funcional

--- sumatoria.py
+++ sumatoria.py
@@ -1,6 +1,11 @@
 def sumatoria_cubica(n):
-    raise NotImplementedError()
+	a = 0
+    for i in range(1,n+2) 
+    	for j in range(1,i)
+    		for k in range(j,(i+j))
+    			a = a + 1
+    return a
 
-def sumatoria_constante(n):
-    raise NotImplementedError()
+
+    
 
